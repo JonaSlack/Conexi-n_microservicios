5e312db6e76b50365db7da6d36b7f0d58d16328c
Victor <vi.salasa@duocuc.cl>
2026-04-24 17:27:16 -0400

    Agrego Model y Repository de hospital

--- src/main/java/com/duoc/hospital/test/Model/HospitalModel.java
+++ src/main/java/com/duoc/hospital/test/Model/HospitalModel.java
@@ -1,4 +1,18 @@
 package com.duoc.hospital.test.Model;
 
-public class HospitalModel {
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "hospitales")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Hospital {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nombre;
+    private String direccion;
 }
--- src/main/java/com/duoc/hospital/test/Repository/HospitalRepository.java
+++ src/main/java/com/duoc/hospital/test/Repository/HospitalRepository.java
@@ -1,4 +1,9 @@
 package com.duoc.hospital.test.Repository;
 
-public interface HospitalRepository {
+import com.duoc.hospital.test.Model.Hospital;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface HospitalRepository extends JpaRepository<Hospital, Long> {
 }
