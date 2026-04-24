b107e0f3702000fc98b47e4dc6790860deb60e9f
Felipe Duran <felip.duranm@duocuc.cl>
2026-04-24 10:16:09 -0400

    agregando Dto

--- src/main/java/com/duoc/hospital/test/Dto/HospitalDto.java
+++ src/main/java/com/duoc/hospital/test/Dto/HospitalDto.java
@@ -1,4 +1,15 @@
 package com.duoc.hospital.test.Dto;
 
-public class HospitalDto {
+package com.duoc.hospital.test.Dto;
+
+import lombok.*;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class HospitalDTO {
+    private Long id;
+    private String nombre;
+    private String direccion;
 }
